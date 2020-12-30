29acb10288820dd299acc4d67513ee80f0dc96d7
Jay Darji <jaydarji369@gmail.com>
2020-12-30 13:30:41 -0500

    Till assignment 2

---
+++ spring5webapp/src/main/java/jd/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,50 @@
+package jd.springframework.spring5webapp.bootstrap;
+
+import jd.springframework.spring5webapp.domain.Author;
+import jd.springframework.spring5webapp.domain.Book;
+import jd.springframework.spring5webapp.domain.Publisher;
+import jd.springframework.spring5webapp.repository.AuthorRepository;
+import jd.springframework.spring5webapp.repository.BookRepository;
+import jd.springframework.spring5webapp.repository.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+//Used to detect as spring managed component by Spring
+@Component
+public class BootStrapData implements CommandLineRunner {
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author amish = new Author("Amish","Tripathi");
+        Book meluha = new Book("Imortal of melhua","1234");
+        amish.getBooks().add(meluha);
+        meluha.getAuthors().add(amish);
+
+        authorRepository.save(amish);
+        bookRepository.save(meluha);
+
+        Author jkr = new Author("JK","Rowlling");
+        Book hp = new Book("Harry Potter","123");
+        jkr.getBooks().add(hp);
+        hp.getAuthors().add(jkr);
+
+        authorRepository.save(jkr);
+        bookRepository.save(hp);
+
+        Publisher penguin = new Publisher("Penguin Random House","131 Lake street","Jersey City","NJ","07306");
+        publisherRepository.save(penguin);
+
+        System.out.println("Bootstrap stated");
+        System.out.println("No of Books: "+bookRepository.count());
+        System.out.println("No of Publisher: "+publisherRepository.count());
+    }
+}
---
+++ spring5webapp/src/main/java/jd/springframework/spring5webapp/repository/PublisherRepository.java
@@ -0,0 +1,7 @@
+package jd.springframework.spring5webapp.repository;
+
+import jd.springframework.spring5webapp.domain.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+}
