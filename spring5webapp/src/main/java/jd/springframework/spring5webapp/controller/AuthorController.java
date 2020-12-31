1bed910bd0eec492f58c6f6dec0154412a56b81f
Jay Darji <jaydarji369@gmail.com>
2020-12-31 16:41:26 -0500

    Display List Author

---
+++ spring5webapp/src/main/java/jd/springframework/spring5webapp/controller/AuthorController.java
@@ -0,0 +1,23 @@
+package jd.springframework.spring5webapp.controller;
+
+import jd.springframework.spring5webapp.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    private String getAuthor(Model model){
+        model.addAttribute("authors",authorRepository.findAll());
+        return "authors/list";
+    }
+}
